cc4bac62632bd7e369ab63e432cb673afdbc257e
RadoslawMilner <rmilner1@hotmail.com>
2022-01-26 14:10:43 +0100

    7.2 Resourceful Routing - read - zakonczone

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  get "/articles", to: "articles#index"
-  get "home/index"
+  root "articles#index"
+  resources :articles
+  # get "home/index"
   # Defines the root path route ("/")
   # root "articles#index"
-  root to: "home#index"
+  # root to: "home#index"
 end
